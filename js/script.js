1ea873a63d183138e30ab6ef46eaaac3b2e43e1b
Anastasia <nastyae08@mail.ru>
2021-11-19 05:53:46 +1000

    Добавили карусель

--- js/script.js
+++ js/script.js
@@ -40,8 +40,6 @@ function cleanClass() {
 const items = document.querySelectorAll('.task-list__column-item');
 const checkbox = document.querySelectorAll('#checkbox');
 
-// Event Listener:
-
 for (let box of checkbox) {
    box.addEventListener('change', function(event) {
       
@@ -49,3 +47,49 @@ for (let box of checkbox) {
 
    });
 }
+
+
+
+//carusel
+const sidebar = document.querySelector(".sidebar");
+const mainSlide = document.querySelector('.main-slide');
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+const slideLength = mainSlide.querySelectorAll("div").length;
+const container = document.querySelector('.sidebar-container');
+
+sidebar.style.top = `-${(slideLength-1)*100}vh`;
+
+let activeSlideIndex = 0;
+
+upBtn.addEventListener('click', () =>{
+   changeSlide('up');
+});
+
+downBtn.addEventListener('click', () =>{
+   changeSlide('down');
+});
+
+function changeSlide(direction) {
+
+   if (direction === 'up') {
+      activeSlideIndex++;
+      if (activeSlideIndex === slideLength) {
+         activeSlideIndex = 0;
+      }
+   }
+
+   if (direction === 'down') {
+      activeSlideIndex--;
+      if (activeSlideIndex < 0) {
+         activeSlideIndex = slideLength-1;
+      }
+   }
+
+   const height = container.clientHeight;
+
+   mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+   sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+}
+
+
